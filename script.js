2d29f92914435cfce6b40177fd67aee0ee55efa4
RuchikaGupta11 <110559974+RuchikaGupta11@users.noreply.github.com>
2023-08-25 17:41:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+
+addTaskBtn.addEventListener("click", addTask);
+
+function addTask() {
+  const taskText = taskInput.value.trim();
+  if (taskText !== "") {
+    const taskItem = document.createElement("li");
+    taskItem.innerHTML = `
+      <span>${taskText}</span>
+      <button class="delete-btn">X</button>
+    `;
+    taskList.appendChild(taskItem);
+    taskInput.value = "";
+
+    const deleteBtn = taskItem.querySelector(".delete-btn");
+    deleteBtn.addEventListener("click", () => {
+      taskList.removeChild(taskItem);
+    });
+
+    taskItem.addEventListener("click", () => {
+      taskItem.classList.toggle("completed");
+    });
+  }
+}
